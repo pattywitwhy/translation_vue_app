73ca18079bbd27a9b886e10c17d0d5eae0fb8827
pattywitwhy <pattyxhwang@gmail.com>
2019-03-11 18:59:38 -0500

    login, logout, signup, and messages index created

---
+++ src/views/MessagesIndex.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="messages-index">
+    <h1>Messages</h1>
+    <div v-for="message in messages">
+      <h2>{{ message.message_id }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+var axios = require('axios');
+
+export default {
+  data: function() {
+    return {
+      messages: []
+    };
+  },
+  created: function() {
+    axios.get("/api/messages").then(response => {
+      this.messages = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
